ab21f24c39e4b502c168d46fee9c3863b7a6d494
zeroflag <amagyar@cloudera.com>
2022-11-15 22:42:08 +0100

    renames

--- make.bat
+++ make.bat
@@ -1,18 +1,19 @@
 del out
 del src
 del make.log
-del forth.com
-debug.com < forth.asm > out
+del dforth.com
+debug.com < dforth.asm > out
 echo "out generated"
-copy forth.asm src
+copy dforth.asm src
 echo w 100 >> src
 debug.com < src
 del src
 echo "bin generated"
 type core.fth > in.fth
 type asm.fth >> in.fth
+type armagdn.fth >> in.fth
 type test.fth >> in.fth
 echo bye >> in.fth
-forth.com < in.fth > make.log
+dforth.com < in.fth > make.log
 del in.fth
 type make.log
